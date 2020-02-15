20feb6b01836cf2c4dcb4c851148bed7f97f15fc
Alouie412 <alouie412@gmail.com>
2020-02-15 07:57:39 +0000

    Initial commit: Implemented basic console

---
+++ console.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+import cmd
+
+
+class HBNBCommand(cmd.Cmd):
+    prompt = "(hbnb) "
+
+    def do_quit(self, line):
+        """Quit command to exit the program
+        """
+        return True
+
+    def do_EOF(self, line):
+        """EOF command to exit the program
+        """
+        return True
+
+    def do_greet(self, line):
+        print("Ohaithar")
+
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
