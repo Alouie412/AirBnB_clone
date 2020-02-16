6796aeebea74fae0f67eb82682f6e8496b848d31
Alouie412 <alouie412@gmail.com>
2020-02-16 07:58:51 +0000

    Modified output to JSON

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """ This file imports code from json, datetime, and pathlib """
-import json
-import datetime
+import json, datetime, os, sys
+#sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath('base_model'))))
+sys.path.append('..')
 from pathlib import Path
 """ FileStorage class. Stores stuff into a JSON file """
 
@@ -24,6 +25,26 @@ class FileStorage:
 
     def save(self):
         """ Save method. Saves/serializes the dictionary to our JSON file """
+        print("Now testing\n")
+        print(self.__objects)
+        print("\nThe end\n")
+        for key, value in self.__objects.items():
+            temp = self.__objects[key]
+            print(temp)
+            try:
+                temp = temp.split(" ")
+                temp = temp[2:]
+                temp = ' '.join(temp)
+                print(temp)
+                self.__objects.update({key: temp})
+            except:
+                print("Failed try")
+                #pass
+
+        print("Now Testing again\n")
+        print(self.__objects)
+        print("\nThe end\n")
+
         with open(self.__file_path, 'w') as f:
             return json.dump(self.__objects, f,
                              default=lambda x: getattr(x, '__dict__',
@@ -42,7 +63,13 @@ class FileStorage:
     def reload(self):
         """ Reload method. Loads/deserializes the dictionary from
         our JSON file """
-        dumped_dict = {}
+        import models
+        new_dict = {}
         if Path(self.__file_path).is_file():
             with open(self.__file_path, 'r') as f:
-                self.__objects = json.load(f, object_hook=FileStorage.date_hook)
+                dumped_dict = json.load(f, object_hook=FileStorage.date_hook)
+                for key, value in dumped_dict.items():
+                    copy = None
+                    copy = key.split('.')
+                    new_dict[key] = "[{}] ({}) {}".format(copy[0], copy[1], value)
+                self.__objects = new_dict
