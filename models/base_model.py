cdba373228d47a8e1a8a5448605e5930533fc9fd
Alouie412 <alouie412@gmail.com>
2020-02-19 08:19:23 +0000

    Logic rewrite: conversion to dictionary

--- models/base_model.py
+++ models/base_model.py
@@ -10,9 +10,9 @@ import models
 class BaseModel:
     """ BaseModel class. The foundation for other classes """
     def __init__(self, *args, **kwargs):
-        """ Initialization method. Creates 5 objects.
-        Supports args and kwargs, as well as conversion between string
-        and datetime. Also creates a new instance """
+        """ Initialization method. Check if kwargs exist. If so,
+        set the instance. Otherwise, create a new instance
+        """
         if len(kwargs) is not 0:
             for key, value in kwargs.items():
                 if key == 'created_at' or key == 'updated_at':
@@ -38,8 +38,7 @@ class BaseModel:
 
     def to_dict(self):
         """ to_dict method. Converts to dictionary format """
-        self.__dict__ = {'__class__': __class__.__name__,
-                         'updated_at': str(self.updated_at.isoformat()),
-                         'id': self.id,
-                         'created_at': str(self.created_at.isoformat())}
-        return self.__dict__
+        to_new_dict = self.__dict__.copy()
+        to_new_dict['created_at'] = self.created_at.isoformat()
+        to_new_dict['updated_at'] = self.updated_at.isoformat()
+        return to_new_dict
