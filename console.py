5b540954c2e06bcbcc6fa8475fafe0dc75e90014
Alouie412 <alouie412@gmail.com>
2020-02-19 21:49:09 +0000

    Added more imports

--- console.py
+++ console.py
@@ -4,6 +4,12 @@ import cmd
 import json
 from models import storage
 from models.base_model import BaseModel
+from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
 """ console.py """
 
 class_list = ['BaseModel', 'User', 'State', 'City', 'Amenity', 'Place',
