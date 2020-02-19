5616fc6f1c53fe11b6bd4d78ca73e66e8881aff4
Alouie412 <alouie412@gmail.com>
2020-02-19 08:09:55 +0000

    Absolute rewrite: Use of eval + removed __init__

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -5,16 +5,19 @@ import json, datetime, os, sys
 sys.path.append('..')
 from pathlib import Path
 from models.base_model import BaseModel
+from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
 """ FileStorage class. Stores stuff into a JSON file """
 
 
 class FileStorage:
     """ FileStorage class """
-    def __init__(self):
-        """ Initialization method. Designates file to be saved to
-        and creates an empty dictionary """
-        self.__file_path = "file.json"
-        self.__objects = {}
+    __file_path = "file.json"
+    __objects = {}
 
     def all(self):
         """ All method. Simply returns the dictionary """
@@ -22,44 +25,19 @@ class FileStorage:
 
     def new(self, obj):
         """ New method. Fills in our dictionary """
-        self.__objects[obj.__class__.__name__ + '.' + obj.id] = obj.__dict__
+        self.__objects[obj.__class__.__name__ + '.' + obj.id] = obj
 
     def save(self):
         """ Save method. Saves/serializes the dictionary to our JSON file """
-        for key, value in self.__objects.items():
-            temp = self.__objects[key]
-            try:
-                temp = temp.split(" ")
-                temp = temp[2:]
-                temp = ' '.join(temp)
-                self.__objects.update({key: temp})
-            except:
-                pass
-
         with open(self.__file_path, 'w') as f:
-            return json.dump(self.__objects, f,
-                             default=lambda x: getattr(x, '__dict__',
-                                                       str(x.isoformat())))
-
-    def date_hook(json_dict):
-        """ Date_hook method. Used in reload method to convert
-        the string in ISO format to datetime format """
-        for key, value in json_dict.items():
-            try:
-                json_dict[key] = datetime.datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
-            except:
-                pass
-        return json_dict
+            new_dict = {key: obj.to_dict() for key, obj in
+                        self.__objects.items()}
+            json.dump(new_dict, f)
 
     def reload(self):
         """ Reload method. Loads/deserializes the dictionary from
         our JSON file """
-        new_dict = {}
         if Path(self.__file_path).is_file():
             with open(self.__file_path, 'r') as f:
-                dumped_dict = json.load(f, object_hook=FileStorage.date_hook)
-                for key, value in dumped_dict.items():
-                    copy = None
-                    copy = key.split('.')
-                    new_dict[key] = "[{}] ({}) {}".format(copy[0], copy[1], value)
-                self.__objects = new_dict
+                for key, value in json.load(f).items():
+                    self.__objects[key] = eval(key.split('.')[0])(**value)
